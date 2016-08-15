60504550c609409bafeebe264783e568bb58be89
Zion <neurcomancer@mailinator.com>
2016-08-14 23:02:17 -0300

    Ajustando settings com DOWNLOAD_DELAY.

--- proxies/proxies/settings.py
+++ proxies/proxies/settings.py
@@ -20,7 +20,7 @@ NEWSPIDER_MODULE = 'proxies.spiders'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
-
+DOWNLOAD_DELAY = 1.5
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
 
